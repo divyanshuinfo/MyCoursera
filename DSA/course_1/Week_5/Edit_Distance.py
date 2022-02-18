1e61deb8f8a3fea794e6c183681c6901f7c22e5d
Divyanshu <56086382+Divyanshu-info@users.noreply.github.com>
2022-02-18 20:21:11 +0530

    18 feb 22

---
+++ DSA/course_1/Week_5/Edit_Distance.py
@@ -0,0 +1,7 @@
+def compute(str1, str2):
+    matrix = [[float("inf")]*len(str2)+1 for _ in range(len(str1) + 1)]
+ 
+    for j in range(len(str2)+1):
+        matrix[len(str1)]
+    num = 0
+    return num
--- DSA/course_1/Week_5/Primitive_Calculator.py
+++ DSA/course_1/Week_5/Primitive_Calculator.py
@@ -1,101 +1,40 @@
-from math import inf
+import math
 
-
-def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return sequence
-
-
-def recur(n):
-    steps = []
-    steps.append(n)
-
-    while n > 0:
-        first_value = inf
-        second_value = inf
-        third_value = inf
-        first_value2 = inf
-        second_value2 = inf
-        third_value2 = inf
-        if n % 3 == 0:
-            first_value = n // 3
-            first_value2 = len(optimal_sequence(first_value))
-
-        if n % 2 == 0:
-            second_value = n // 2
-            second_value2 = len(optimal_sequence(second_value))
-
-        if first_value == inf and second_value == inf:
-            n = n - 1
-            steps.append(n)
-            continue
-        else:
-            third_value = n - 1
-            third_value2 = len(optimal_sequence(third_value))
-
-        d = min(first_value2, second_value2, third_value2)
-
-        if d == second_value2:
-            steps.append(second_value)
-            n = second_value
-        elif d == first_value2:
-            steps.append(first_value)
-            n = first_value
-        elif d == third_value2:
-            steps.append(third_value)
-            n = third_value
-
-    return len(steps)-2, reversed(steps[:-1])
-
-
-def recur2(n):
-    steps = []
-    good_solution = optimal_sequence(n)
-    for i in range(len(good_solution)-1):
-        g = good_solution[i]
-        v1, v2, v3 = inf, inf, inf
-        a1, a2, a3 = inf, inf, inf
-        b1, b2, b3 = inf, inf, inf
-        v1 = g - 1
-        a1 = optimal_sequence(v1)
-        b1 = len(a1)
-        if g % 3 == 0:
-            v2 = g // 3
-            a2 = optimal_sequence(v2)
-            b2 = len(a2)
-        if g % 2 == 0:
-            v3 = g // 2
-            a3 = optimal_sequence(v3)
-            b3 = len(a3)
-            
-        d = min(b1, b2, b3)
-        if d == b2 and v2 != good_solution[i+1]:
-            good_solution[i+1:] = optimal_sequence(v2)
-        elif d == b3 and v3 != good_solution[i+1]:
-            good_solution[i+1:] = optimal_sequence(v3)
-        elif d == b1 and v1 != good_solution[i+1]:
-            good_solution[i+1:] = optimal_sequence(v1)
-        
-    return len(good_solution), reversed(good_solution)
-    
 
 def compute(n):
+    # number of operations required for getting 0, 1, 2,.. , n
+    num_operations = [0, 0] + [math.inf]*(n-1)
 
-    a, b = recur2(n)
-    return [a, list(b)]
+    for i in range(2, n+1):
+        temp1, temp2, temp3 = [math.inf]*3
+
+        temp1 = num_operations[i-1] + 1
+        if i % 2 == 0:
+            temp2 = num_operations[i//2] + 1
+        if i % 3 == 0:
+            temp3 = num_operations[i//3] + 1
+        min_ops = min(temp1, temp2, temp3)
+        num_operations[i] = min_ops
+    pos = num_operations[n]
+    # Backtracking the numbers leading to n
+    nums = [n]
+    while n != 1:
+        if n % 3 == 0 and num_operations[n]-1 == num_operations[n//3]:
+            nums += [n//3]
+            n = n//3
+        elif n % 2 == 0 and num_operations[n]-1 == num_operations[n//2]:
+            nums += [n//2]
+            n = n//2
+        else:
+            nums += [n-1]
+            n = n - 1
+            reversed(list(map(int, nums)))
+    return ([pos, list(reversed(list(map(int, nums))))])
 
 
 if __name__ == "__main__":
     n = int(input())
-    a, b = compute(n)
-    print(a)
-    for g in b:
-        print(g, end=" ")
+    out = compute(n)
+    print(out[0])
+    for i in out[1]:
+        print(i, end=" ")
